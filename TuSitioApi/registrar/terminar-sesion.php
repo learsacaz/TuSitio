b4f8f74a4407b714f7dd182bc6f3fc1a6631d6d0
LearsacaZ <79977824+learsacaz@users.noreply.github.com>
2021-08-31 09:23:53 -0500

    Backend TuSitio
    Api backend de red social TuSitio

---
+++ TuSitioApi/registrar/terminar-sesion.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+header('Content-Type: text/html; charset=utf-8');
+header('P3P: CP="IDC DSP COR CURa ADMa OUR IND PHY ONL COM STA"');
+header("Content-type: application/json; charset=utf-8");
+require_once('../../APIopenweatherapp/conexion/conexion.php');
+require_once('./user-class.php');
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+    $input = json_decode(file_get_contents("php://input"));
+    $usuario_id = intval($input);
+    $objUser= new User($pdo);
+    $res = $objUser->sesionFinal($usuario_id);
+    
+}
+?>
